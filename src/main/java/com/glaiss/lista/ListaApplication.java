f81e07efa21e09c9ca3e4b7ca0e27f46bbb763cb
Gustavo Acácio <gustavo.acacio@attus.ai>
2024-11-25 20:07:49 -0300

    criacao filtro no elasticsearch

--- src/main/java/com/glaiss/lista/ListaApplication.java
+++ src/main/java/com/glaiss/lista/ListaApplication.java
@@ -1,13 +1,25 @@
 package com.glaiss.lista;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.glaiss.core", "com.glaiss.lista"})
+@EntityScan(basePackages = "com.glaiss.lista.domain.model")
 public class ListaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ListaApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("America/Sao_Paulo"));
 	}
 
 }
